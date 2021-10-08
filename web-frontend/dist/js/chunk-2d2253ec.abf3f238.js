c593b834c2472ab99bac2eb0d1fe0fca98823def
Marcin <242138+mrcne@users.noreply.github.com>
2021-10-08 09:55:04 +0200

    remove manual build

--- web-frontend/dist/js/chunk-2d2253ec.abf3f238.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp"]=this["webpackJsonp"]||[]).push([["chunk-2d2253ec"],{e429:function(e,s,n){e.exports=n.p+"js/stellarium-web-engine.17a58548.wasm"}}]);
-//# sourceMappingURL=chunk-2d2253ec.abf3f238.js.map
